0ee27b784606f8ce6c421b63910d51dfdda78112
Vladislav <84326092+val5idalv@users.noreply.github.com>
2023-04-25 19:51:33 +0300

    Add files via upload

---
+++ SpringSecurityApplication/src/main/java/com/example/springsecurityapplication/controllers/AuthenticationController.java
@@ -0,0 +1,18 @@
+package com.example.springsecurityapplication.controllers;
+
+import com.example.springsecurityapplication.models.Product;
+import com.example.springsecurityapplication.repositories.CategoryRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+
+
+    @GetMapping("/authentication")
+    public String login(){
+        return "authentication";
+    }
+
+}
